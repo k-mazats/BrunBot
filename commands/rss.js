251f698b3280cf05bb8cece7bdb7006b63bdd9d5
Kevin Mazats <k.mazats@gmail.com>
2021-08-25 17:34:12 +0200

    added rss action argument

--- commands/rss.js
+++ commands/rss.js
@@ -2,14 +2,19 @@ import rssListener from '../services/rss/rssListener.js';
 import guildConfig from '../utils/guildConfig.js';
 const rss = {
 	requiredArgs: ['url'],
-	run: async ([url], msg) => {
-		const localConfig = await guildConfig.getConfig(msg.guild.id, 'rss');
-		if (!localConfig[url]) {
-			rssListener.initListener(url, msg.guild.id);
-			msg.reply(`Subscribed to ${url}`);
+	run: async ([url, action], msg) => {
+		switch (action) {
+		case 'add': {
+			const localConfig = await guildConfig.getConfig(msg.guild.id, 'rss');
+			if (!localConfig[url]) {
+				rssListener.initListener(url, msg.guild.id);
+				msg.reply(`Subscribed to ${url}`);
+			}
+			else {
+				msg.reply(`BrunBot is already subscribed to ${url}`);
+			}
+			return;
 		}
-		else {
-			msg.reply(`BrunBot is already subscribed to ${url}`);
 		}
 	},
 	// return true si erreur gerée par la commande

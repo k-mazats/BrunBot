ebebe42a9912a24bf598c4221301d0739efd41a3
Kevin Mazats <k.mazats@gmail.com>
2021-08-25 11:50:39 +0200

    added post and get queries methods

---
+++ utils/query.js
@@ -0,0 +1,35 @@
+const axios = require('axios').default;
+
+const query = {
+	get: async (url, headers = undefined) => {
+		try {
+			const response = await axios({
+				method: 'get',
+				url: url,
+				json: true,
+				headers,
+			});
+			return response;
+		}
+		catch (err) {
+			console.error(err);
+		}
+	},
+	post: async (url, data, headers = undefined) => {
+		try {
+			const response = await axios({
+				method: 'post',
+				url: url,
+				json: true,
+				data,
+				headers,
+			});
+			return response;
+		}
+		catch (err) {
+			console.error(err);
+		}
+	},
+};
+
+export default query;
